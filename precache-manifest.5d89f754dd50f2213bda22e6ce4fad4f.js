801b2f02db0a5bd4c76ee33a3d5768205efcf51e
Michael Frenkel <michael.frenkel@outlook.com>
2019-08-24 19:55:32 -0700

    Updates

---
+++ precache-manifest.5d89f754dd50f2213bda22e6ce4fad4f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62be3b8465986bbc202dc9a77f37f155",
+    "url": "/react-dashboard-demo/index.html"
+  },
+  {
+    "revision": "99fe381bee986dbf5197",
+    "url": "/react-dashboard-demo/static/css/2.1f113664.chunk.css"
+  },
+  {
+    "revision": "08219866836812f17b9e",
+    "url": "/react-dashboard-demo/static/css/main.8b72ed49.chunk.css"
+  },
+  {
+    "revision": "99fe381bee986dbf5197",
+    "url": "/react-dashboard-demo/static/js/2.ce8b6b85.chunk.js"
+  },
+  {
+    "revision": "08219866836812f17b9e",
+    "url": "/react-dashboard-demo/static/js/main.d20c184e.chunk.js"
+  },
+  {
+    "revision": "d84b7976fafc6176fd0e",
+    "url": "/react-dashboard-demo/static/js/runtime~main.e8846a2b.js"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/react-dashboard-demo/static/media/CircularStd-Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/react-dashboard-demo/static/media/CircularStd-Book.6365c40a.otf"
+  },
+  {
+    "revision": "4fcdd97fadc3a1d9887f816f2aa67f1d",
+    "url": "/react-dashboard-demo/static/media/CircularStd-Medium.4fcdd97f.otf"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "/react-dashboard-demo/static/media/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "/react-dashboard-demo/static/media/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "/react-dashboard-demo/static/media/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
+    "url": "/react-dashboard-demo/static/media/fa-brands-400.bfa9c38b.svg"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "/react-dashboard-demo/static/media/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "/react-dashboard-demo/static/media/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "/react-dashboard-demo/static/media/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "95f13e0be408d117bd3d9b366084a3ef",
+    "url": "/react-dashboard-demo/static/media/fa-regular-400.95f13e0b.svg"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "/react-dashboard-demo/static/media/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "/react-dashboard-demo/static/media/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/react-dashboard-demo/static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/react-dashboard-demo/static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "/react-dashboard-demo/static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/react-dashboard-demo/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/react-dashboard-demo/static/media/fa-solid-900.d6d8d5da.woff2"
+  },
+  {
+    "revision": "be2ed1511f856f57648ebb4d1c0a1b96",
+    "url": "/react-dashboard-demo/static/media/user-img-placeholder.be2ed151.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/online-retail-dashboard/precache-manifest.cf6716b09b9d5ff65daebf14bedc6e9b.js"
+  "/react-dashboard-demo/precache-manifest.5d89f754dd50f2213bda22e6ce4fad4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/online-retail-dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-dashboard-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
